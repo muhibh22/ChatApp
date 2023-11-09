2bb6fac599af58eada1b8b1cfea6d432fb0626e0
muhibh22 <muhibh22@gmail.com>
2023-11-09 20:53:51 +0600

    upload

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,6 +4,7 @@ const express = require("express");
 //internal exports
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
 
 const router = express.Router();
 
@@ -11,4 +12,7 @@ const router = express.Router();
 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
+//add user
+
+router.post("/", avatarUpload);
 module.exports = router;
