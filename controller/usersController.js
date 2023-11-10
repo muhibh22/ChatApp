024361fd34660de34deff5f7ee3d8cb478025544
muhibh22 <muhibh22@gmail.com>
2023-11-10 16:02:24 +0600

    user add

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,47 @@
 //getrUsers Page
 
+//external imports
+const bycrypt = require("bcrypt");
+
+//internal imports
+
+const User = require("../models/People");
+
 function getUsers(req, res, next) {
   res.render("users");
 }
 
-module.exports = { getUsers };
+async function addUsers(req, res, next) {
+  let newUser;
+  const hashedPassword = await bycrypt.hash(req.body.password, 10);
+  if (req.files && req.files.length > 0) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+  }
+
+  //save user or send error
+  try {
+    const result = await newUser.save();
+    res.status(200).json({
+      message: "User was added successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: "Unknows error occured",
+        },
+      },
+    });
+  }
+}
+
+module.exports = { getUsers, addUsers };
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,10 +1,15 @@
 //external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 //internal exports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidator");
 
 const router = express.Router();
 
@@ -14,5 +19,11 @@ router.get("/", decorateHtmlResponse("Users"), getUsers);
 
 //add user
 
-router.post("/", avatarUpload);
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUsers
+);
 module.exports = router;
